8d800a3adb826fa3513366e7ba3afb6f8a4830bc
donuthole8 <muro5866@gmail.com>
2022-10-07 06:19:01 +0900

    feat: 詳細ページ

--- src/Controller/Portfolio/BlogController.php
+++ src/Controller/Portfolio/BlogController.php
@@ -10,13 +10,31 @@ use App\Entity\Blog;
 
 class BlogController extends AbstractController
 {
-    #[Route('/portfolio/blog', name: 'app_portfolio_blog')]
+    /**
+     * @Route("/portfolio/blog", name="blog_index")
+     */
     public function index(EntityManagerInterface $em): Response
     {
         $blogs = $em->getRepository(Blog::class)->findAll();
 
         return $this->render('portfolio/blog/index.html.twig', [
             'blogs' => $blogs,
+        ]);
+    }
+    
+    /**
+     * @Route("/portfolio/blog/{id}", name="blog_content")
+     */
+    public function showContentPage($id, EntityManagerInterface $em)
+    {
+        $blog = $em->getRepository(Blog::class)->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('The post does not exist');
+        }
+
+        return $this->render('portfolio/blog/content.html.twig', [
+            'blog' => $blog
         ]);
     }
 }
